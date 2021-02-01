13840f1a1973471c6d30a3a8b652eba9dc165cfe
Marat Mukiyat <m.mukiyat@astanait.edu.kz>
2021-02-01 23:10:03 +0600

    First commit

---
+++ src/Basket.API/Startup.cs
@@ -0,0 +1,84 @@
+using Basket.API.Data;
+using Basket.API.Data.Interface;
+using Basket.API.Repository;
+using Basket.API.Repository.IRepository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            #region Redis dependencies
+            services.AddSingleton<ConnectionMultiplexer>(sp =>
+            {
+                var config = ConfigurationOptions.Parse(Configuration.GetConnectionString("redis"), true);
+                return ConnectionMultiplexer.Connect(config);
+            });
+            #endregion
+
+            #region Project Dependencies
+            services.AddTransient<IBasketCardContext, BasketCardContext>();
+            services.AddTransient<IBasketRepository, BasketRepository>();
+            #endregion
+
+            #region Swagger Dependencies
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Redis Example", Version = "v1" });
+            });
+
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Redis Example V1");
+            });
+        }
+    }
+}
